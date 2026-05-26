58874efe1c88ba62c5282338b4bf5a3b3bae521f
jieling <jieling.works@gmail.com>
2026-05-26 15:51:37 +0800

    feat: add tailwindcss, vue-query, vue-flow and zod deps

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { createApp } from "vue"
+import App from "@/App.vue"
+import router from "@/router/index.ts"
+import { VueQueryPlugin } from "@tanstack/vue-query"
 import { createPinia } from "pinia"
-
-import App from "./App.vue"
-import router from "./router/index.ts"
+import { createApp } from "vue"
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin)
 
 app.mount("#app")
